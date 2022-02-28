8b8c2f88d5155572478144e5148e77027d130d2d
Garis Kuu <garisku05@gmail.com>
2022-02-28 07:53:00 +0000

    test

--- build.sh
+++ build.sh
@@ -1,17 +1,14 @@
 #sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/ArrowOS/android_manifest.git -b arrow-12.0 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/nhAsif/local_manifest.git --depth 1 -b arrow-12 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j10
 
 # build rom
 source build/envsetup.sh
-lunch arrow_rosy-userdebug
-export TZ=Asia/Dhaka
-export BUILD_USERNAME=rosy
-export BUILD_HOSTNAME=userdebug
+export TZ=Asia/Jakarta
 curl -s https://api.telegram.org/bot$TG_TOKEN/sendMessage -d chat_id=$TG_CHAT_ID -d text="$(echo "${var_cache_report_config}")"
 m bacon -j10 &
-sleep 95m
+tmate -S /tmp/tmate.sock new-session -d && tmate -S /tmp/tmate.sock wait tmate-ready && send_shell=$(tmate -S /tmp/tmate.sock display -p '#{tmate_ssh}') && tg $id "Build Failed" && tg $id "$send_shell" && ccache -s && sleep 2h
+sleep 4h
 kill %1
 
 
